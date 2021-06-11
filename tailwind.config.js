b07aec88d60e588a325f421e394f4ac8d8c05332
Robert Jewell <rjewellaudio@gmail.com>
2021-06-11 17:40:51 +0200

    setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+	mode: "jit",
+	purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		fontFamily: {
+			sans: [
+				"DM Sans",
+				"ui-sans-serif",
+				"system-ui",
+				"-apple-system",
+				"BlinkMacSystemFont",
+				"Segoe UI",
+				"Roboto",
+				"Helvetica Neue",
+				"Arial",
+				"Noto Sans",
+				"sans-serif",
+				"Apple Color Emoji",
+				"Segoe UI Emoji",
+				"Segoe UI Symbol",
+				"Noto Color Emoji",
+			],
+		},
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
