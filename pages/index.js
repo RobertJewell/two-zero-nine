412118d7d70ac5f9fb4fd69844ad99d3607a48e2
Robert Jewell <rjewellaudio@gmail.com>
2021-06-22 17:15:44 +0200

    add gradient and initial landing section

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import Head from "next/head";
 import NavBar from "../components/NavBar";
+import Landing from "../components/Landing";
 
 export default function Home() {
 	return (
 		<div className="">
+			<div className="relative">
+				<div className="absolute w-full featureGradient h-96 -z-10"></div>
+			</div>
 			<NavBar></NavBar>
+			<Landing></Landing>
 		</div>
 	);
 }
