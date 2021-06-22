f8116cc7eb284d2c33eb062f4ad56013107a0f53
Robert Jewell <rjewellaudio@gmail.com>
2021-06-22 17:15:24 +0200

    update navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Image from "next/image";
 import Link from "next/link";
 import TwoZeroNineLogo from "../public/assets/images/TwoZeroNineLogo.svg";
+import Button from "./button";
 
 const NavBar = () => {
 	return (
@@ -12,37 +12,48 @@ const NavBar = () => {
 				<div className="w-6 h-1 bg-pink-500 rounded-full "></div>
 			</div>
 			<div className="container mx-auto text-white">
-				<div className="flex flex-row items-center justify-between h-16 mx-auto text-sm ">
+				<div className="flex flex-row items-center justify-between h-20 mx-auto text-sm ">
 					<Link href="/">
-						<a className="w-24 p-2 text-center">
+						<a className="w-24 p-4 text-center">
 							<TwoZeroNineLogo></TwoZeroNineLogo>
 						</a>
 					</Link>
 					<ul className="flex flex-row items-center justify-around w-1/2">
-						<li className="w-24 p-2 text-center">
+						<li className="flex">
 							<Link href="/">
-								<a className="w-24 p-2 text-center">Services</a>
+								<a className="w-24 p-2 text-center transition-colors hover:text-accent-main">
+									Services
+								</a>
 							</Link>
 						</li>
-						<li className="w-24 p-2 text-center">
+						<li className="flex">
 							<Link href="/">
-								<a className="w-24 p-2 text-center">About</a>
+								<a className="w-24 p-2 text-center transition-colors hover:text-accent-main">
+									About
+								</a>
 							</Link>
 						</li>
-						<li className="w-24 p-2 text-center">
+						<li className="flex">
 							<Link href="/">
-								<a className="w-24 p-2 text-center">Portfolio</a>
+								<a className="w-24 p-2 text-center transition-colors hover:text-accent-main">
+									Portfolio
+								</a>
 							</Link>
 						</li>
-						<li className="w-24 p-2 text-center">
+						<li className="flex">
 							<Link href="/">
-								<a className="w-24 p-2 text-center">FAQ</a>
+								<a className="w-24 p-2 text-center transition-colors hover:text-accent-main">
+									FAQ
+								</a>
 							</Link>
 						</li>
 					</ul>
-					<div className="flex flex-row justify-between w-24 p-2 text-center">
-						<p>Contact</p>
-					</div>
+					<Button
+						text="Contact"
+						type="secondary"
+						width="w-24"
+						link="/"
+					></Button>
 				</div>
 			</div>
 		</div>
