947e34290d2723e29a1881c673ddef7fb0f574eb
Robert Jewell <rjewellaudio@gmail.com>
2021-06-25 19:19:28 +0200

    create navbar button

---
+++ components/MobileDropdownButton.js
@@ -0,0 +1,37 @@
+import { motion } from "framer-motion";
+
+const top = {
+	open: { y: 10, rotate: -45 },
+	closed: { y: 0 },
+};
+const middle = {
+	open: { opacity: 0 },
+	closed: { opacity: 1 },
+};
+const bottom = {
+	open: { y: -10, rotate: 45 },
+	closed: { y: 0 },
+};
+
+export default function MobileDropdownButton({ open }) {
+	return (
+		<div className="block p-4 bg-white rounded-full outline-none bg-opacity-20 sm:hidden">
+			<motion.div
+				animate={open ? "open" : "closed"}
+				variants={top}
+				className="w-6 h-1 mb-1.5 bg-accent-muted rounded-full "
+			></motion.div>
+			<motion.div
+				animate={open ? "open" : "closed"}
+				transition={{ duration: 0.1 }}
+				variants={middle}
+				className="w-6 h-1 mb-1.5 bg-accent-muted rounded-full "
+			></motion.div>
+			<motion.div
+				animate={open ? "open" : "closed"}
+				variants={bottom}
+				className="w-6 h-1 rounded-full bg-accent-muted "
+			></motion.div>
+		</div>
+	);
+}
