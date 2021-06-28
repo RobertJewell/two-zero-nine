137742f8112410423e1d72b1d0265b8e3a3afa4a
Robert Jewell <rjewellaudio@gmail.com>
2021-06-28 18:32:10 +0200

    add dark and image variants

--- components/Button.js
+++ components/Button.js
@@ -7,6 +7,9 @@ export default function Button({ type, link, text, width, margin, extras }) {
 			"p-2 text-center align-middle transition-colors bg-white border-2 border-white text-gray-900 rounded-lg hover:bg-gray-200 hover:border-gray-200",
 		secondary:
 			"p-2 text-center align-middle transition-colors border-2 border-gray-200 rounded-lg hover:bg-gray-800",
+		secondaryDark:
+			"p-2 text-dark text-center align-middle transition-colors border-2 border-dark rounded-lg hover:bg-gray-100",
+		image: "align-middle",
 	};
 
 	const classNames = `${width} ${buttonType[type]} `;
