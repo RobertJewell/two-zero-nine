bce43d2b1dfc5da0406e374137c79128ec4f2bd2
Robert Jewell <rjewellaudio@gmail.com>
2021-06-29 18:39:23 +0200

    initial plan setup

---
+++ components/ThreeStepPlan.js
@@ -0,0 +1,13 @@
+export default function ThreeStepPlan() {
+	return (
+		<div className="container flex flex-col items-center justify-center w-full px-4 mx-auto mt-16 text-white">
+			<h1 className="text-6xl font-bold">How it works</h1>
+			<p className="max-w-xl mt-6 text-sm leading-6 text-center">
+				We work with musicians & engineers all over the world, so even if you’re
+				not local to our mastering studio in Liverpool, you can still use our
+				online mastering services. We’ve made it really easy for you to get your
+				music ready for release:
+			</p>
+		</div>
+	);
+}
