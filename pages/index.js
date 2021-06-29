bc289293418e1117614965957e2f299d84241334
Robert Jewell <rjewellaudio@gmail.com>
2021-06-29 18:39:06 +0200

    add gradient

--- pages/index.js
+++ pages/index.js
@@ -4,26 +4,28 @@ import NavBar from "../components/NavBar";
 import Landing from "../components/Landing";
 import BrandLogos from "../components/BrandLogos";
 import AboutSection from "../components/AboutSection";
+import ThreeStepPlan from "../components/ThreeStepPlan";
 
 export default function Home() {
 	return (
-		<div className="relative overflow-x-hidden">
-			{/* Decoration */}
-			{/* Top Gradient */}
+		<div className="overflow-hidden">
 			<div className="relative">
+				{/* Top Gradient */}
 				<div className="absolute w-full featureGradient h-96 -z-10"></div>
-			</div>
-			{/* Top Vinyl */}
-			<div className="relative">
+				{/* Top Vinyl */}
 				<Vinyl className="absolute transform scale-150 rotate-90 w-600 md:w-800 translate-x-2/4 sm:-rotate-90 sm:-translate-x-2/4 -z-10 sm:-translate-y-2/4 stroke-8"></Vinyl>
-			</div>
 
-			{/* Content */}
-			<div className="mx-4">
 				<NavBar></NavBar>
+
 				<Landing></Landing>
+
 				<BrandLogos></BrandLogos>
+				{/* Gradient */}
+				<div className="absolute w-full transform -translate-y-60 -z-10 middleGradient--green h-800 opacity-60"></div>
+
 				<AboutSection></AboutSection>
+
+				<ThreeStepPlan></ThreeStepPlan>
 			</div>
 		</div>
 	);
