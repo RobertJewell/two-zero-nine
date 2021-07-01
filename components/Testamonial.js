a4b1f3c1a13120e8a7f1ef0339cade5e6d77cf0c
Robert Jewell <rjewellaudio@gmail.com>
2021-07-01 18:49:06 +0200

    fix drop shadow

--- components/Testamonial.js
+++ components/Testamonial.js
@@ -5,7 +5,7 @@ export default function Testamonial() {
 	return (
 		<div className="container flex justify-center px-4 mx-auto mt-32">
 			<div className="flex flex-col items-center justify-center w-full max-w-3xl px-8 bg-gray-100 rounded-lg ">
-				<div className="w-32 h-32 -mb-16 transform -translate-y-16 rounded-full drop-shadow-xl">
+				<div className="w-32 h-32 -mb-16 overflow-visible transform -translate-y-16 rounded-full drop-shadow-xl">
 					<Image
 						className="w-32 h-32 rounded-full"
 						src={testamonialPhoto}
