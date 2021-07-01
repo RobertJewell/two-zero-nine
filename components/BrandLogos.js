4bb587dcb5d4a44c312f3e16c31eea8dfde2e347
Robert Jewell <rjewellaudio@gmail.com>
2021-07-01 18:48:53 +0200

    add kerrang and radioX logos

--- components/BrandLogos.js
+++ components/BrandLogos.js
@@ -1,17 +1,21 @@
 import Neve from "../public/assets/images/logos/Neve.svg";
-import Showcase from "../public/assets/images/logos/Showcase.svg";
 import Sky from "../public/assets/images/logos/Sky.svg";
 import BBC from "../public/assets/images/logos/BBC.svg";
+import Kerrang from "../public/assets/images/logos/kerrang.svg";
+import RadioX from "../public/assets/images/logos/radioX.svg";
+// import Showcase from "../public/assets/images/logos/Showcase.svg";
 
 export default function BrandLogos() {
 	return (
 		<div className="flex flex-col items-center justify-center w-full mt-12 sm:flex-row">
 			<div className="flex mt-12">
 				<Neve className="w-32 h-16 mx-4"></Neve>
-				<Showcase className="w-32 h-16 mx-4"></Showcase>
+				<RadioX className="relative w-32 h-16 mx-4 top-1"></RadioX>
+				{/* <Showcase className="w-32 h-16 mx-4"></Showcase> */}
 			</div>
 			<div className="flex mt-12">
-				<Sky className="w-32 h-16 mx-4"></Sky>
+				<Sky className="hidden w-32 h-16 mx-4 lg:block"></Sky>
+				<Kerrang className="w-32 h-16 mx-4"></Kerrang>
 				<BBC className="w-32 h-16 mx-4"></BBC>
 			</div>
 		</div>
