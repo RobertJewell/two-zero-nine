ede827196475d7c6426948e5c7d1cd1c3827cf2f
Robert Jewell <rjewellaudio@gmail.com>
2021-07-06 12:55:46 +0200

    change secondary option styling

--- components/OptionsItem.js
+++ components/OptionsItem.js
@@ -2,7 +2,7 @@ export default function OptionsItem({ title, description }) {
 	return (
 		<div className="flex flex-col mt-12">
 			<h3 className="text-2xl font-bold text-white">{title}</h3>
-			<p className="mt-4 leading-6 text-gray-200">{description}</p>
+			<p className="mt-4 leading-6 text-gray-300">{description}</p>
 		</div>
 	);
 }
--- components/ProductSecondary.js
+++ components/ProductSecondary.js
@@ -9,7 +9,7 @@ export default function ProductSecondary({
 	return (
 		<div className="container flex flex-col items-start w-full px-4 mx-auto text-white">
 			<div
-				className={`relative w-full px-8 py-10 mx-auto bg-gray-800 max-w-400 min-h-300 sm:px-12 rounded-xl ${classes}`}
+				className={`relative w-full px-8 py-10 mx-auto bg-clip-padding backdrop-filter bg-gray-700 backdrop-blur-md bg-opacity-30 border-4 border-gray-300 max-w-400 min-h-300 sm:px-12 rounded-xl ${classes}`}
 			>
 				<h2 className="text-2xl font-bold ">{title}</h2>
 				<p className="mt-6 leading-6">{description}</p>
