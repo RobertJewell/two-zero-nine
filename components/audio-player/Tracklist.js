7bad31ca598e3bddcc7e7a7071a76a776e69e291
Robert Jewell <rjewellaudio@gmail.com>
2021-08-05 15:13:58 +0200

    inital tracklist commit

---
+++ components/audio-player/Tracklist.js
@@ -0,0 +1,10 @@
+import tracks from "./tracklist.json";
+
+export default function Tracklist({ tracksJSON }) {
+	return (
+		<div>
+			<div className="">hello</div>
+			{console.log(tracksJSON)}
+		</div>
+	);
+}
--- pages/listen.js
+++ pages/listen.js
@@ -3,6 +3,8 @@ import Footer from "../components/Footer";
 import GradientBG from "../components/decoration/GradientBG";
 import PageTitle from "../components/PageTitle";
 import AudioPlayer from "../components/audio-player/AudioPlayer";
+import tracksJSON from "../components/audio-player/tracklist.json";
+import Tracklist from "../components/audio-player/Tracklist";
 
 export default function listen() {
 	return (
@@ -21,6 +23,8 @@ export default function listen() {
 				<PageTitle title="Listen" classes="max-w-4xl px-4 lg:px-0"></PageTitle>
 
 				<AudioPlayer albumart="/assets/images/dave-pensado.png"></AudioPlayer>
+
+				<Tracklist tracksJSON={tracksJSON}></Tracklist>
 
 				<Footer></Footer>
 			</div>
