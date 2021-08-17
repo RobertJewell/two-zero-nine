7d635a096dddf431f8c4f67adb25d17db43ca4fd
Robert Jewell <rjewellaudio@gmail.com>
2021-08-17 14:55:24 +0100

    add explicit tag options

--- components/audio-player/Track.js
+++ components/audio-player/Track.js
@@ -1,14 +1,23 @@
 import React from "react";
 
-export default function Track({ trackName, artist, location, selectTrack }) {
+export default function Track({ trackName, artist, selectTrack, explicit }) {
 	return (
 		<li
 			className="flex-col py-4 list-none"
 			onClick={() => selectTrack(trackName)}
 		>
-			<div className="p-2 px-4 transition-colors rounded-lg cursor-pointer hover:bg-gray-800">
-				<p className="text-lg font-bold text-white">{trackName}</p>
-				<p className="text-gray-300">{artist}</p>
+			<div className="flex p-2 px-4 transition-colors rounded-lg cursor-pointer hover:bg-gray-800">
+				<div className="flex-col flex-1">
+					<p className="text-lg font-bold text-white">{trackName}</p>
+					<p className="text-gray-300">{artist}</p>
+				</div>
+				{explicit && (
+					<div className="flex items-center">
+						<p className="text-sm font-bold text-yellow-500 rounded-lg">
+							Explicit
+						</p>
+					</div>
+				)}
 			</div>
 		</li>
 	);
