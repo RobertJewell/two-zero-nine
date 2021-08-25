e7dd9da96d7ff7b26c5b0f4ed134daf7d8dd6929
Robert Jewell <rjewellaudio@gmail.com>
2021-08-25 16:55:40 +0200

    initial landing page animation

--- components/BrandLogos.js
+++ components/BrandLogos.js
@@ -4,10 +4,21 @@ import BBC from "../public/assets/images/logos/bbc1.svg";
 import Kerrang from "../public/assets/images/logos/kerrang.svg";
 import RadioX from "../public/assets/images/logos/radioX.svg";
 // import Showcase from "../public/assets/images/logos/Showcase.svg";
+import { motion } from "framer-motion";
 
 export default function BrandLogos() {
 	return (
-		<div className="flex flex-col items-center justify-center w-full mt-12 sm:flex-row">
+		<motion.div
+			className="flex flex-col items-center justify-center w-full mt-12 sm:flex-row"
+			initial={{
+				opacity: 0,
+			}}
+			animate={{
+				y: 0,
+				opacity: 1,
+			}}
+			transition={{ delay: 1, duration: 1 }}
+		>
 			<div className="flex mt-12">
 				<Neve className="w-32 h-16 mx-4"></Neve>
 				<RadioX className="relative w-32 h-16 mx-4 top-1"></RadioX>
@@ -18,6 +29,6 @@ export default function BrandLogos() {
 				<Kerrang className="w-32 h-16 mx-4"></Kerrang>
 				<BBC className="w-32 h-16 py-1 mx-4"></BBC>
 			</div>
-		</div>
+		</motion.div>
 	);
 }
--- components/Landing.js
+++ components/Landing.js
@@ -1,17 +1,48 @@
 import Button from "./Button";
+import { motion } from "framer-motion";
 
 export default function Landing() {
 	return (
 		<div className="container flex flex-col justify-center w-full px-4 mx-auto text-white min-h-96 h-screen-60 sm:min-h-400 sm:h-screen-60 max-h-700 sm:items-center sm:px-0 ">
-			<h1 className="pt-12 text-6xl font-bold text-white sm:text-center sm:text-8xl ">
+			<motion.h1
+				className="pt-12 text-6xl font-bold text-white sm:text-center sm:text-8xl"
+				initial={{
+					opacity: 0,
+				}}
+				animate={{
+					y: 0,
+					opacity: 1,
+				}}
+				transition={{ duration: 0.3 }}
+			>
 				Your <br className="sm:hidden" /> sound:
 				<br />
 				<span className="text-accent-main">Perfected</span>
-			</h1>
-			<h3 className="pt-4 sm:text-center">
+			</motion.h1>
+			<motion.h3
+				className="pt-4 sm:text-center"
+				initial={{
+					opacity: 0,
+				}}
+				animate={{
+					y: 0,
+					opacity: 1,
+				}}
+				transition={{ delay: 0.4, duration: 0.4 }}
+			>
 				Audio mastering in the heart of Liverpool.
-			</h3>
-			<div className="flex items-center pt-16 sm:justify-center">
+			</motion.h3>
+			<motion.div
+				className="flex items-center pt-16 sm:justify-center"
+				initial={{
+					opacity: 0,
+				}}
+				animate={{
+					y: 0,
+					opacity: 1,
+				}}
+				transition={{ delay: 0.8, duration: 0.4 }}
+			>
 				<Button
 					text="Get started"
 					type="primary"
@@ -28,7 +59,7 @@ export default function Landing() {
 					link="/services"
 					extras="hidden sm:flex"
 				></Button>
-			</div>
+			</motion.div>
 		</div>
 	);
 }
