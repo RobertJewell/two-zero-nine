db8743bc174d182daaab7c5c4d1f8b61fe407771
RobertJewell <59702435+RobertJewell@users.noreply.github.com>
2021-08-27 12:46:38 +0200

    add home button

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,4 @@
+import Button from "./Button";
 
 
 export default function Custom404 () {
@@ -5,9 +6,17 @@ export default function Custom404 () {
         <div className="pt-20 overflow-hidden">
             <div className="relative py-16 grid place-content-center text-center">
                 
-                <h1 className="text-accent-main font-bold">404 ERROR</h1>
-                <p className="text-white font-bold text-4xl">Huh, weird</p>
-                <p className="text-white">It looks like this page doesn't exist</p>
+                <h1 className="text-accent-main font-bold mb-2">404 ERROR</h1>
+                <p className="text-white font-bold text-4xl mb-4">Huh, weird</p>
+                <p className="text-white mb-8">It looks like this page doesn't exist</p>
+                <Button
+					text="Take me home"
+					type="primary"
+					width="w-40"
+					margin="sm:mx-4"
+					link="/"
+					extras=""
+				></Button>
             </div>
         </div>
 
