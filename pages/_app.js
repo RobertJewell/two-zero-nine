fb5ebe1954ec6ca7ef47dee56152497f57948551
Robert Jewell <rjewellaudio@gmail.com>
2021-08-31 13:48:13 +0200

    add favicons

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import Head from "next/head";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import { AnimatePresence, motion } from "framer-motion";
@@ -7,6 +8,37 @@ import GradientBG from "../components/decoration/GradientBG";
 function MyApp({ Component, pageProps, router }) {
 	return (
 		<>
+			<Head>
+				<title>
+					Two Zero Nine Mastering | Professional Audio Mastering, Liverpool
+				</title>
+				<meta
+					name="description"
+					content="Mastering solutions for musicians, labels and studios around the world from our dedicated studio in Liverpool."
+				/>
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/site.webmanifest" />
+				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0a0e15" />
+				<meta name="msapplication-TileColor" content="#00aba9" />
+				<meta name="theme-color" content="#0a0e15" />
+			</Head>
 			<NavBar></NavBar>
 			{router.route !== "/" && (
 				<GradientBG
